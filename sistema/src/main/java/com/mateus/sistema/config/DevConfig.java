8713d543e25e7fdcd5e06ff3391fbbc468fe9d69
mateusrdg <mateusrodrigues-cp@hotmail.com>
2019-12-14 12:25:09 -0300

    Alterações na config

--- sistema/src/main/java/com/mateus/sistema/config/DevConfig.java
+++ sistema/src/main/java/com/mateus/sistema/config/DevConfig.java
@@ -1,19 +1,31 @@
 package com.mateus.sistema.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.mateus.sistema.services.DBService;
 
 @Configuration
+@Profile("dev")
 public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy; 
+	
 	@Bean
-	public void instantiateDatabase () {
+	public Boolean instantiateDatabase () {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
+		return true;
 	}
 }
